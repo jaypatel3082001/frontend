10083823a30f050a17b3a244f2b7128d78874338
nobislinkdev <101082815+nobislinkdev@users.noreply.github.com>
2024-07-15 17:23:48 +0530

    ssss

--- src/componet/User/Quizestart.js
+++ src/componet/User/Quizestart.js
@@ -524,7 +524,7 @@ function Quizestart({ id, keyid }) {
                                     <input
                                       type="radio"
                                       sectionId={ele._id}
-                                      sectionname={ele.quizename}
+                                      sectionname={ele.quizname}
                                       weightage={ele.weightage}
                                       name={`option-${partIndex}-${questionIndex}`}
                                       value={`option${index + 1}`}
