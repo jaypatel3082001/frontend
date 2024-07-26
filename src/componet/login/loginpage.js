a16669d13f109546764b60046bef7fc666a9e450
nobislinkdev <101082815+nobislinkdev@users.noreply.github.com>
2024-07-26 10:01:43 +0530

    sasasas

--- src/componet/login/loginpage.js
+++ src/componet/login/loginpage.js
@@ -3,7 +3,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../cssfile/Login.css";
-import { SignUpUser, Studentloging } from "../../services/auth";
+import { Studentloging } from "../../services/auth";
 
 const Loginpage = () => {
   const initialdata = {
